3448bc81f29a56bc255598bcb2802f3bb1ec2b1d
WibblyWobbly <simon.radionov@gmail.com>
2019-03-10 14:37:58 +0200

    add sounds

--- src/Page.js
+++ src/Page.js
@@ -1,14 +1,57 @@
-import React, { Component } from 'react';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from "native-base";
+import Sound from "react-native-sound";
+
+// Enable playback in silence mode
+Sound.setCategory("Playback");
 
 export default class Page extends Component {
+  componentDidMount() {
+    // Load the sound file 'whoosh.mp3' from the app bundle
+    // See notes below about preloading sounds within initialization code below.
+    var whoosh = new Sound("duck.mp3", Sound.MAIN_BUNDLE, error => {
+      if (error) {
+        console.log("failed to load the sound", error);
+        return;
+      }
+      // loaded successfully
+      console.log(
+        "duration in seconds: " +
+          whoosh.getDuration() +
+          "number of channels: " +
+          whoosh.getNumberOfChannels()
+      );
+
+      // Play the sound with an onEnd callback
+      whoosh.play(success => {
+        if (success) {
+          console.log("successfully finished playing");
+        } else {
+          console.log("playback failed due to audio decoding errors");
+        }
+      });
+    });
+  }
   render() {
     return (
       <Container>
         <Header>
           <Left>
             <Button transparent>
-              <Icon name='menu' />
+              <Icon name="menu" />
             </Button>
           </Left>
           <Body>
@@ -17,9 +60,7 @@ export default class Page extends Component {
           <Right />
         </Header>
         <Content>
-          <Text>
-            This is Content Section
-          </Text>
+          <Text>This is Content Section 23</Text>
         </Content>
         <Footer>
           <FooterTab>
