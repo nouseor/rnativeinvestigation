025b5cb9bb61b19b7226c88f943695e37aec9566
anastasia <hotredmulled@gmail.com>
2019-03-10 16:03:28 +0200

    add mouse

--- src/Page.js
+++ src/Page.js
@@ -49,6 +49,33 @@ export default class Page extends Component {
     });
   }
 
+  playMouse() {
+    // Load the sound file 'whoosh.mp3' from the app bundle
+    // See notes below about preloading sounds within initialization code below.
+    var whoosh = new Sound("mouse.mp3", Sound.MAIN_BUNDLE, error => {
+      if (error) {
+        console.log("failed to load the sound", error);
+        return;
+      }
+      // loaded successfully
+      console.log(
+        "duration in seconds: " +
+          whoosh.getDuration() +
+          "number of channels: " +
+          whoosh.getNumberOfChannels()
+      );
+
+      // Play the sound with an onEnd callback
+      whoosh.play(success => {
+        if (success) {
+          console.log("successfully finished playing");
+        } else {
+          console.log("playback failed due to audio decoding errors");
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <Container>
@@ -67,6 +94,12 @@ export default class Page extends Component {
           <Button onPress={this.handleClick}>
             <Image
               source={require('./images/duck.jpeg')}
+              style={styles.ImageIconStyle}
+            />
+          </Button>
+          <Button onPress={this.playMouse}>
+            <Image
+              source={require('./images/mouse.jpeg')}
               style={styles.ImageIconStyle}
             />
           </Button>
